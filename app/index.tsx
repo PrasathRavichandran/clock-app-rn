44b92c277a33dc3c0d5afb170ab0156fce4715e2
prasath webby <prasath.webby@gmail.com>
2025-06-12 08:57:01 +0530

    Added custom font in the app.

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,14 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={Styles.title}>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+const Styles = StyleSheet.create({
+  title: {
+    fontFamily: "Avenir-Light",
+    fontSize: 30,
+  },
+});
